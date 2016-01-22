ab6b91a4cfe479a3e0600c047befc029f0ab38fc
bast <bast@users.noreply.github.com>
2016-01-22 22:06:25 +0100

    change the example

--- example.py
+++ example.py
@@ -1,18 +1,30 @@
-def add_numbers(f1, f2):
-    return f1 + f2
+def get_word_lengths(s):
+    """
+    Returns a list of integers representing
+    the word lengths in string s.
+    """
+    return [len(w) for w in s.split()]
 
 
-def test_add_numbers():
-    assert (add_numbers(1.0, 2.0) == 3.0)
+def test_get_word_lengths():
+    text = "Three tomatoes are walking down the street"
+    assert get_word_lengths(text) == [5, 8, 3, 7, 4, 3, 6]
+
+# ------------------------------------------------------------------------------
 
 
-def subtract_numbers(f1, f2):
-    return f1 - f2
-
-
-def test_subtract_numbers():
-    assert (subtract_numbers(1.0, 2.0) == -1.0)
-
-
-def multiply_numbers(f1, f2):
-    return f1*f2
+# example of a function which is never tested
+# see coverage analysis
+def pythagorean_triples(n):
+    """
+    Returns list of all unique pythagorean triples
+    (a, b, c) where a < b < c <= n.
+    """
+    l = []
+    # loop over all a < b < c <= n
+    for c in range(1, n + 1):
+        for b in range(1, c):
+            for a in range(1, b):
+                if a*a + b*b == c*c:
+                    l.append((a, b, c))
+    return l
