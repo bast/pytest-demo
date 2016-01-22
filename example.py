3b1e03e1e03ddad0d03210a6172ea8ce1b4ddf03
bast <bast@users.noreply.github.com>
2016-01-22 21:58:46 +0100

    create just one file for simplicity

--- example.py
+++ example.py
@@ -1,9 +1,18 @@
-from example import *
+def add_numbers(f1, f2):
+    return f1 + f2
 
 
 def test_add_numbers():
     assert (add_numbers(1.0, 2.0) == 3.0)
 
 
+def subtract_numbers(f1, f2):
+    return f1 - f2
+
+
 def test_subtract_numbers():
     assert (subtract_numbers(1.0, 2.0) == -1.0)
+
+
+def multiply_numbers(f1, f2):
+    return f1*f2
